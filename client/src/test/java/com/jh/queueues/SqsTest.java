3215d13fcb71f684d9025d168246b6dd81dbc710
Josiah Haswell <josiah.haswell@gmail.com>
2018-03-28 13:59:47 -0600

    Adding dependencies

---
+++ client/src/test/java/com/jh/queueues/SqsTest.java
@@ -0,0 +1,16 @@
+package com.jh.queueues;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+@ExtendWith(SpringExtension.class)
+public class SqsTest {
+
+
+    @Test
+    void testSomething() {
+    }
+}
